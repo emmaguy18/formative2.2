6740978678413d46d81a948a9df07beab5f14ab0
Emma Guy <932400387@bun2-206-24.local>
2019-06-07 11:47:50 +1200

    added content

--- js/script.js
+++ js/script.js
@@ -1,5 +1,69 @@
 $(document).ready(function(){
-  $('#modalPlanner').on('shown.bs.modal', function () {
-    $('#bttn-planner').trigger('focus')
+var days= [
+  {
+    id:1,
+    day:'monday',
+    taskName:[],
+    category:['chores', 'appointments','important','social']
+  }
+]
+for (var i = 0; i < days.length; i++) {
+  var nameValue= $('#taskName')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// hide and show new task popup//
+$('#bttn-addTask').on('click', function () {
+  $('#modalPlanner-form').show();
+});
+
+$('.close').on('click', function() {
+  $('#modalPlanner-form').hide();
+});
+
+
+
+
+// hide and show planner//
+  $('#bttn-planner').on('click', function () {
+    $('#modalPlanner').show();
   });
+
+  $('.close').on('click', function() {
+    $('#modalPlanner').hide();
+  });
+
+
+
+
+
+
+
+
 });
